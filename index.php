fb1eba920d23df410dd44d95243f5d9e40f6ab00
Wellington Silvestre <wellingtonsroberto@gmail.com>
2017-03-27 09:51:23 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,146 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+
+    <meta charset="UTF-8" />
+    <title>Intelecto</title>
+
+
+<link rel="stylesheet" type="text/css" href="css/style.css"/>
+<link href="https://fonts.googleapis.com/css?family=Hind:400,700" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Archivo+Narrow:400,700" rel="stylesheet">
+</head>
+<body>	
+<!------------------>
+<!-- BLOCO TOPO -->
+<!------------------>
+<div class="desk">
+<div class="bloco_topo">
+	<img src="images/logo.png">
+	<span>CONTABILIDADE <br> PARA CONDOMÍNIOS
+    <img class="img_divisao" src="images/especial.png">
+	</span>
+</div>
+</div>
+<div class="div_baixo_desk">
+<img class="div_baixo" src="images/baixo.png">
+<!------------------>
+<!-- BLOCO VANTAGEM -->
+<!------------------>
+
+<div class="bloco_vantagem">
+	<h2>GESTÃO FINANCEIRA</h2>
+	<li>Elaboração dos boletos de taxas condominiais com demonstrativo de receitas e despesas (prestação de contas mensal);</li>
+	<li>Emissão e envio de boleto Impresso e por e-mail;</li>
+	<li>Pagamentos de todas as despesas de forma on-line;
+    </li>
+	<li>Gestão das contas de Aplicação Financeira e Poupança (Fundos de Reserva);
+    </li>
+	<li>Cobrança e protesto dos inadimplentes sem custo adicional para o condomínio;</li>
+</div>
+</div>
+<!------------------>
+<!-- BLOCO GESTÃO -->
+<!------------------>
+
+<div class="bloco_gestao">
+	<img src="images/card.png">
+	<h2>DEPARTAMENTO<br>PESSOAL</h2>
+    <ol type="1">
+	<li>Calculo da folha de Pagamentos/Rescisões/Férias dos empregados;</li>
+	<b><li>Controle de Vales Transportes, Vales Alimentações, Seguro de vida etc;</li></b>
+	<li>Contrato de experiência; Comunicação de admissão e demissão ao TEM/CAIXA/INSS/RFB etc.;</li>
+	<b><li>Carta de apresentação de empregados; Cálculo dos tributos e contribuições previdenciárias, cálculo do FGTS/SEFIP; </li></b>
+	<li>Apresentação da RAIS – Relação Anual de Informações Sociais e da GFIP – Informações previdenciárias, preenchimento da Declaração da DIRF;</li>
+	<b><li>Acompanhamento de homologação de rescisão no sindicato da categoria;
+</li></b>
+	<li>Atendimento das demais exigências previstas na Legislação, bem como, de eventuais procedimentos contábeis.</li>
+</ol>
+</div>
+
+<!------------------>
+<!-- BLOCO FORM -->
+<!------------------>
+
+<div class="bloco_form">
+	<span>Quer receber um artigo sobre os <b>princípios básicos para atingir o sucesso como síndico</b> de condomínio? Deixe suas informações abaixo!
+	</span>
+    <div class="form1">
+      <span>
+      <input type="text" placeholder="Nome">
+      <input type="text" placeholder="Email">
+      <input type="text" placeholder="Celular">
+      </span>
+    <button><b>ENVIAR</b></button>
+          </div>
+    </div>
+
+<!------------------>
+<!-- BLOCO DEPART -->
+<!------------------>
+
+<div class="bloco_depart">
+		<img src="images/news.png">
+	<h2>VANTAGENS</h2>
+    <ol type="1">
+	<li>Calculo da folha de Pagamentos/Rescisões/Férias dos empregados;</li>
+	<b><li>Controle de Vales Transportes, Vales Alimentações, Seguro de vida etc;</li></b>
+	<li>Contrato de experiência; Comunicação de admissão e demissão ao TEM/CAIXA/INSS/RFB etc.;</li>
+	<b><li>Carta de apresentação de empregados; Cálculo dos tributos e contribuições previdenciárias, cálculo do FGTS/SEFIP; </li></b>
+	<li>Apresentação da RAIS – Relação Anual de Informações Sociais e da GFIP – Informações previdenciárias, preenchimento da Declaração da DIRF;</li>
+	<b><li>Acompanhamento de homologação de rescisão no sindicato da categoria;
+</li></b>
+	<li>Atendimento das demais exigências previstas na Legislação, bem como, de eventuais procedimentos contábeis.</li>
+</ol>
+</div>
+
+<!------------------>
+<!-- BLOCO FORM 2 -->
+<!------------------>
+
+	<div class="bloco_form2">
+	<span>Quer receber uma cartilha com os <b>5 passos para o sucesso como síndico de condomínio?</b> Deixe suas informações abaixo!
+	</span>
+    <div class="form2">
+      <span>
+      <input type="text" placeholder="Nome">
+      <input type="text" placeholder="Email">
+      <input type="text" placeholder="Celular">
+      </span>
+    <button><b>ENVIAR</b></button>
+    </div>
+    <div class="img_mini">
+        <img src="images/mini.png">
+</div>
+</div>
+
+<!------------------>
+<!-- BLOCO RODAPÉ -->
+<!------------------>
+
+<div class="bloco_rodape">
+	<svg class="svg_face" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 167.657 167.657" style="enable-background:new 0 0 167.657 167.657;" xml:space="preserve" width="30px" height="30px">
+<g>
+	<path d="M83.829,0.349C37.532,0.349,0,37.881,0,84.178c0,41.523,30.222,75.911,69.848,82.57v-65.081H49.626   v-23.42h20.222V60.978c0-20.037,12.238-30.956,30.115-30.956c8.562,0,15.92,0.638,18.056,0.919v20.944l-12.399,0.006   c-9.72,0-11.594,4.618-11.594,11.397v14.947h23.193l-3.025,23.42H94.026v65.653c41.476-5.048,73.631-40.312,73.631-83.154   C167.657,37.881,130.125,0.349,83.829,0.349z" fill="#FFFFFF"/>
+</g>
+<span>
+fb.com/intelectocont
+</span>
+<span>
+<b>(43) 3323-5691</b>
+<br>
+<b>contato@intelectocont.com.br</b>
+</span>
+<span>
+R. Senador Souza Naves, nº 289,<br>
+Sala 33. Centro, Londrina - PR
+</span>
+<span>
+www.intelectocont.com.br
+</span>
+</div>
+</div>
+
+
+</body>
+</html>
